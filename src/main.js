cf7178ff8339944bf33dddc418de8a2affe1947e
Huyen <nguyenmanhhuyen1997@gmail.com>
2022-09-21 15:25:57 +0700

    first commit

---
+++ src/main.js
@@ -0,0 +1,42 @@
+$(".teacher_owlcarousel").owlCarousel({
+    margin: 10,
+    loop: true,
+    autoplay: true,
+    nav: true,
+    autoplayTimeout: 1500,
+    dots: true,
+    autoplayHoverpause: true,
+    responsive: {
+        0: {
+            items: 2,
+            nav: false,
+        },
+        600: {
+            items: 2,
+            nav: false,
+        },
+        1000: {
+            items: 4,
+            nav: false,
+        },
+    },
+});
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 40,
+    autoplay: true,
+    autoplayTimeout: 1500,
+
+    dots: true,
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 2,
+        },
+        1000: {
+            items: 3,
+        },
+    },
+});
