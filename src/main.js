ec6bf8d840c5ae76ae4fd2bf2dc9d3cdad005053
Huyen <nguyenmanhhuyen1997@gmail.com>
2022-09-22 11:50:10 +0700

    commit 5

--- src/main.js
+++ src/main.js
@@ -40,3 +40,21 @@ $(".owl-carousel").owlCarousel({
         },
     },
 });
+
+function certificate_content1() {
+    document.getElementById("certificate_content2").style.display = "flex";
+    document.getElementById("certificate_content1").style.display = "none";
+    document.getElementById("subtitle_content-1").style.color = "#2881bb";
+    document.getElementById("subtitle_content-1").style.fontWeight = "700";
+    document.getElementById("subtitle_content-2").style.fontWeight = "unset";
+    document.getElementById("subtitle_content-2").style.color = "black";
+}
+
+function certificate_content2() {
+    document.getElementById("certificate_content1").style.display = "flex";
+    document.getElementById("certificate_content2").style.display = "none";
+    document.getElementById("subtitle_content-2").style.color = "#2881bb";
+    document.getElementById("subtitle_content-2").style.fontWeight = "700";
+    document.getElementById("subtitle_content-1").style.fontWeight = "unset";
+    document.getElementById("subtitle_content-1").style.color = "black";
+}
